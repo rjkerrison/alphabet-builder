01d6cc35ecca9577d20784e3c3be71a3e398fade
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-12-10 13:04:16 +0000

    WIP extensions

--- src/alphabetBuilder.js
+++ src/alphabetBuilder.js
@@ -4,11 +4,22 @@ function cleanup(alphabet, letter, bestSeed) {
       continue
     }
 
-    var allLettersCoveredTwice = seed.everyLetters(seedLetter => alphabet.getSeeds(seedLetter).size > 1)
+    var allLettersCoveredTwice = seed.everyLetters(
+      (seedLetter) => alphabet.getSeeds(seedLetter).size > 1
+    )
 
     if (allLettersCoveredTwice) {
-      seed.everyLetters(seedLetter => console.log(seedLetter, alphabet.getSeeds(seedLetter).size, Array.from(alphabet.getSeeds(seedLetter)).map(seed => seed.name)) || true)
-      console.log(`${seed.name} is unnecessary now that ${bestSeed.name} has been added.`)
+      seed.everyLetters(
+        (seedLetter) =>
+          console.log(
+            seedLetter,
+            alphabet.getSeeds(seedLetter).size,
+            Array.from(alphabet.getSeeds(seedLetter)).map((seed) => seed.name)
+          ) || true
+      )
+      console.log(
+        `${seed.name} is unnecessary now that ${bestSeed.name} has been added.`
+      )
       seed.letterSet.forEach(function (seedLetter) {
         var letterSeeds = alphabet.getSeeds(seedLetter)
         letterSeeds.delete(seed)
@@ -33,7 +44,7 @@ function processNextLetter(alphabet, bestSeed) {
 }
 
 function operate(alphabet, chooseBestSeed) {
-  while (alphabet.some(x => !x.found)) {
+  while (alphabet.some((x) => !x.found)) {
     var bestSeed = chooseBestSeed(alphabet)
     if (!bestSeed) {
       console.log('bestSeed', bestSeed)
@@ -43,16 +54,24 @@ function operate(alphabet, chooseBestSeed) {
     processNextLetter(alphabet, bestSeed)
   }
 
-  var usedSeeds = alphabet.vocabulary.filter(x => x.used)
+  var usedSeeds = alphabet.vocabulary.filter((x) => x.used)
 
-  console.log(usedSeeds.map(x => {
-    return {
-      name: x.name,
-      uniqueLetters: x.filterLetters(letter => alphabet.getSeeds(letter).size == 1)
-    }
-  }))
+  console.log(
+    usedSeeds.map((x) => {
+      return {
+        name: x.name,
+        uniqueLetters: x.filterLetters(
+          (letter) => alphabet.getSeeds(letter).size == 1
+        ),
+      }
+    })
+  )
 
-  console.log(`${alphabet.filter(z => z.found).length} letters found in ${usedSeeds.length} seeds.`)
+  console.log(
+    `${alphabet.filter((z) => z.found).length} letters found in ${
+      usedSeeds.length
+    } seeds.`
+  )
 }
 
 module.exports = operate
--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 const choosers = require('./choosers')
 const operate = require('./alphabetBuilder')
 
+const Alphabet = require('./Alphabet')
+const Seed = require('./Seed')
+
+function stringArrayToAlphabet(stringArray) {
+  return new Alphabet(stringArray.map((x) => new Seed(x)))
+}
+
+const seeds = stringArrayToAlphabet([
+  `Robin wrote this program whose very intention is just to examine a list of` +
+    ' strings to quickly determine which is best at generating an alphabet.',
+  ' which just provides numerous executable algorithms for quickly analyzing sentences.',
+  ' It gives the user a quick summary of which string covers' +
+    '  the entire alphabet with the least duplication.',
+  `The quick brown fox jumps over the lazy dog.`,
+  `What happens here? Can it go and quickly realize the alphabet from just one string?`,
+])
+
+const buildByMostLetters = (alphabet) =>
+  operate(alphabet, choosers.chooseByMostLetters)
+
+const buildByLowestAverageFrequency = (alphabet) =>
+  operate(alphabet, choosers.chooseByLowestAverageFrequency)
+
+buildByMostLetters(seeds)
+buildByLowestAverageFrequency(seeds)
+
 module.exports = {
-  buildByMostLetters: alphabet => operate(alphabet, choosers.chooseByMostLetters),
-  buildByLowestAverageFrequency: alphabet => operate(alphabet, choosers.chooseByLowestAverageFrequency),
-  buildByRandom: alphabet => operate(alphabet, choosers.chooseByRandom)
+  buildByMostLetters: buildByMostLetters,
+  buildByLowestAverageFrequency: (alphabet) =>
+    operate(alphabet, choosers.chooseByLowestAverageFrequency),
+  buildByRandom: (alphabet) => operate(alphabet, choosers.chooseByRandom),
+  stringArrayToAlphabet: stringArrayToAlphabet,
 }
